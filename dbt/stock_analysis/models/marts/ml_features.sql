98ffee1616fdbab49f0d280a0401dd9b0d6360b0
Pol John Cruz <poljohncruz@gmail.com>
2026-01-31 10:34:55 +1030

    Fixed few issues in marts and intermediate

--- dbt/stock_analysis/models/marts/ml_features.sql
+++ dbt/stock_analysis/models/marts/ml_features.sql
@@ -1,7 +1,15 @@
 {{ config(materialized='table') }}
 
-SELECT
+WITH base as (SELECT
     *,
-    LEAD(CLOSE, 1) OVER (PARTITION BY TICKER ORDER BY DATETIME) AS NEXT_PRICE
+    LEAD(CLOSE, 1) OVER (PARTITION BY TICKER ORDER BY DATETIME) AS NEXT_PRICE,
 FROM   
     {{ ref('int_price_features') }}
+    )
+
+SELECT
+    *,
+    CASE WHEN NEXT_PRICE > CLOSE THEN 1 ELSE 0 END AS LABEL
+FROM   
+    base
+WHERE NEXT_PRICE IS NOT NULL
